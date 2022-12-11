d837cea618ce0b6b7584782e1a62a10ededf899c
Jon Rutter <contact@jonrutter.io>
2022-12-11 02:10:14 -0500

    Create basic redux slice for tasks

---
+++ src/features/tasks/store/tasksSlice.ts
@@ -0,0 +1,107 @@
+// TODO: subscribe to tag updating/deleting actions
+
+import { createSlice } from '@reduxjs/toolkit';
+
+// utils
+import { nanoid } from 'nanoid';
+
+// types
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '@/store/store';
+import type { TagType } from '@/features/tags';
+
+// types
+
+export type PriorityType = 1 | 2 | 3 | 4;
+
+export interface TaskIncompleteType {
+  name: string;
+  description: string;
+  priority: PriorityType;
+  due?: Date;
+  tag?: TagType;
+  date?: Date;
+  id?: string;
+  completed?: boolean;
+}
+
+export interface TaskType extends TaskIncompleteType {
+  date: Date;
+  id: string;
+  completed: boolean;
+}
+
+type TaskState = TaskType[];
+
+const initialState: TaskState = [];
+
+export const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action: PayloadAction<TaskIncompleteType>) => {
+      const incompleteTask = action.payload;
+      const newTask = {
+        date: new Date(),
+        ...incompleteTask,
+        id: nanoid(),
+        completed: false,
+      };
+      state.push(newTask);
+    },
+    updateTask: (
+      state,
+      action: PayloadAction<{ id: string; data: TaskIncompleteType }>
+    ) => {
+      const { id, data } = action.payload;
+      // updating the list with the given id with the new data
+      return state.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              ...data,
+            }
+          : item
+      );
+    },
+    markTaskCompleted: (state, action: PayloadAction<string>) => {
+      const id = action.payload;
+      return state.map((item) =>
+        item.id === id ? { ...item, completed: true } : item
+      );
+    },
+    unmarkTaskCompleted: (state, action: PayloadAction<string>) => {
+      const id = action.payload;
+      return state.map((item) =>
+        item.id === id ? { ...item, completed: false } : item
+      );
+    },
+    deleteCompletedTasks: (state) => {
+      return state.filter((item) => !item.completed);
+    },
+  },
+});
+
+// export actions
+export const {
+  addTask,
+  updateTask,
+  markTaskCompleted,
+  unmarkTaskCompleted,
+  deleteCompletedTasks,
+} = tasksSlice.actions;
+
+// export reducer
+export const { reducer } = tasksSlice;
+
+// export selectors
+export const selectList = (state: RootState) =>
+  state.tasks.filter((i) => !i.completed);
+
+export const selectCompleted = (state: RootState) =>
+  state.tasks.filter((i) => i.completed);
+
+export const selectTask = (id: string) => (state: RootState) =>
+  state.tasks.find((item) => item.id === id);
+
+export const selectTaskIds = (state: RootState) => state.tasks.map((i) => i.id);
