3a3bbf1467f9851aba7c08329bf93347d8fedb95
Jon Rutter <contact@jonrutter.io>
2022-12-12 17:25:03 -0500

    Re-export from tasks redux slice

--- src/features/tasks/index.ts
+++ src/features/tasks/index.ts
@@ -1,3 +1,4 @@
+// components
 export { Task } from './components/Task';
 export { CompletedTask } from './components/CompletedTask';
 export { CreateTask } from './components/CreateTask';
@@ -5,3 +6,6 @@ export { TaskCreateDialog } from './components/TaskCreateDialog';
 export { TaskCreateDropdown } from './components/TaskCreateDropdown';
 export { TaskList } from './components/TaskList';
 export { CompletedTaskList } from './components/CompletedTaskList';
+
+// state
+export * from './store/tasksSlice';
