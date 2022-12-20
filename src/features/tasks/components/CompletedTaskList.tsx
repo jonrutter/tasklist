564fdba685b845a6183d6a17606fbca25a1fed6f
Jon Rutter <contact@jonrutter.io>
2022-12-20 16:35:18 -0500

    Refactor completed tasks
    - Move state down to CompletedTaskList
    
    - Render CompletedTasks by id

--- src/features/tasks/components/CompletedTaskList.tsx
+++ src/features/tasks/components/CompletedTaskList.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 // components
-import { List, Typography, Box } from '@mui/material';
+import { List, Typography, Box, Button } from '@mui/material';
 import { CompletedTask } from './CompletedTask';
+import { WarningDialog } from '@/components/ui/WarningDialog';
+
+// icons
+import Delete from '@mui/icons-material/Delete';
+
+// hooks
+import { useDispatch, useSelector } from '@/app';
+import { usePopup } from '@/hooks/usePopup';
+
+// utils
+import { isTaskCompleted } from '@/utils/filters';
 
 // types
-import type { TaskType } from '../store/tasksSlice';
+import {
+  deleteCompletedTasks,
+  selectFilteredTaskIds,
+} from '../store/tasksSlice';
 
 type Props = {
-  list: TaskType[];
   label: string;
 };
 
@@ -16,19 +29,55 @@ type Props = {
  * A modified TaskList, for rendering completed tasks.
  */
 export const CompletedTaskList: React.FC<Props> = ({
-  list = [],
   label = 'Completed tasks',
-}) => (
-  <Box sx={{ pt: 1 }}>
-    <Typography variant="h6" component="h2">
-      {list.length <= 0 ? 'No completed tasks' : label}
-    </Typography>
-    {list.length > 0 && (
-      <List>
-        {list.map((task) => (
-          <CompletedTask key={task.id} task={task} />
-        ))}
-      </List>
-    )}
-  </Box>
-);
+}) => {
+  const dispatch = useDispatch();
+
+  const filter = useCallback(isTaskCompleted, []);
+  const [warningOpen, openWarning, closeWarning] = usePopup(false);
+
+  const deleteAll = () => {
+    dispatch(deleteCompletedTasks());
+    closeWarning();
+  };
+  const completedTaskIds = useSelector(selectFilteredTaskIds(filter));
+
+  const checkBeforeWarning = () => {
+    if (completedTaskIds.length <= 0) return;
+    else openWarning();
+  };
+
+  return (
+    <>
+      <Box sx={{ pt: 1 }}>
+        <Typography variant="h6" component="h2">
+          {completedTaskIds.length <= 0 ? 'No completed tasks' : label}
+        </Typography>
+        {completedTaskIds.length > 0 && (
+          <List>
+            {completedTaskIds.map((id) => (
+              <CompletedTask key={id} id={id} />
+            ))}
+          </List>
+        )}
+      </Box>
+      <Button
+        onClick={checkBeforeWarning}
+        color="error"
+        startIcon={<Delete />}
+        sx={{ mt: 1 }}
+      >
+        Delete All
+      </Button>
+      <WarningDialog
+        open={warningOpen}
+        title="Empty Trash"
+        body="Are you sure you want to delete all of your completed tasks? This can't be undone."
+        handleCancel={closeWarning}
+        handleConfirm={deleteAll}
+        cancelLabel="Cancel"
+        confirmLabel="Delete"
+      />
+    </>
+  );
+};
--- src/pages/CompletedPage.tsx
+++ src/pages/CompletedPage.tsx
@@ -3,66 +3,15 @@ import React from 'react';
 // react helmet
 import { Helmet } from 'react-helmet-async';
 
-// mui
-import { Button } from '@mui/material';
-import { Delete } from '@mui/icons-material';
-
-// components
-import { WarningDialog } from '@/components/ui/WarningDialog';
-
 // tasks
-import {
-  deleteCompletedTasks,
-  selectCompleted,
-  CompletedTaskList,
-} from '@/features/tasks';
+import { CompletedTaskList } from '@/features/tasks';
 
-// store
-import { useSelector, useDispatch } from '@/app';
+export const CompletedPage: React.FC = () => (
+  <>
+    <Helmet>
+      <title>Completed | TaskList</title>
+    </Helmet>
 
-// custom hooks
-import { usePopup } from '@/hooks/usePopup';
-
-export const CompletedPage: React.FC = () => {
-  const dispatch = useDispatch();
-  const completedList = useSelector(selectCompleted);
-
-  const [warningOpen, openWarning, closeWarning] = usePopup(false);
-
-  const deleteAll = () => {
-    dispatch(deleteCompletedTasks());
-    closeWarning();
-  };
-
-  const checkBeforeWarning = () => {
-    if (!completedList.length) return;
-    else openWarning();
-  };
-
-  return (
-    <>
-      <Helmet>
-        <title>Completed | TaskList</title>
-      </Helmet>
-
-      <CompletedTaskList list={completedList} label="Completed" />
-      <Button
-        onClick={checkBeforeWarning}
-        color="error"
-        startIcon={<Delete />}
-        sx={{ mt: 1 }}
-      >
-        Delete All
-      </Button>
-      <WarningDialog
-        open={warningOpen}
-        title="Empty Trash"
-        body="Are you sure you want to delete all of your completed tasks? This can't be undone."
-        handleCancel={closeWarning}
-        handleConfirm={deleteAll}
-        cancelLabel="Cancel"
-        confirmLabel="Delete"
-      />
-    </>
-  );
-};
+    <CompletedTaskList label="Completed" />
+  </>
+);
