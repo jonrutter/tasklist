b209c5f24cc9f14fd70ff82a453f66bd104e9d57
Jon Rutter <contact@jonrutter.io>
2023-01-26 14:05:32 -0500

    Configure vitest
    - Add test setup
    
    - Add vitest globals to tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './test/setupTests.js',
+    setupFiles: './test/setup.js',
   },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
